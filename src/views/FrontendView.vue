db18f07043d908951fdc4a1333e21a3e2a69db29
IanMcbull <ianmugenya@gmail.com>
2023-01-16 10:38:38 +0300

    Project updatse

--- src/views/FrontendView.vue
+++ src/views/FrontendView.vue
@@ -7,7 +7,90 @@
     <p class="text-sm mt-2">A simple QR Code Generator built with Vuejs, Tailwindcss and Typescript</p>
     <div class="flex space-x-3 mt-6">
      <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
+     <a href="https://spectacular-elf-3624f8.netlify.app/" class="underline text-orange-500" target="_blank">Live Link</a>
+    </div>
+   </div>
+  </div>
+  <div class="rounded-md flex flex-col shadow-xl">
+   <img src="https://media.giphy.com/media/ITsSZQpHM2Is8VGXfq/giphy.gif" alt="QR Generator Application Image"/>
+   <div class="p-4">
+    <h2 class="text-2xl underline underline-offset-2">Rick and Morty </h2>
+    <p class="text-sm mt-2">An app that allows the user to search for different characters from the Rick and Morty show.</p>
+    <div class="flex space-x-3 mt-6">
+     <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
      <a href="https://superlative-moonbeam-329608.netlify.app/#/" class="underline text-orange-500" target="_blank">Live Link</a>
+    </div>
+   </div>
+  </div>
+
+  <div class="rounded-md flex flex-col shadow-xl">
+   <img src="https://media.giphy.com/media/uMZnovciMDdTLqL8W0/giphy.gif" alt="QR Generator Application Image"/>
+   <div class="p-4">
+    <h2 class="text-2xl underline underline-offset-2">Web 3 Frontend Mentor</h2>
+    <p class="text-sm mt-2">Landing page for a Web 3 news agency</p>
+    <div class="flex space-x-3 mt-6">
+     <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
+     <a href="https://curious-frangipane-02d009.netlify.app/" class="underline text-orange-500" target="_blank">Live Link</a>
+    </div>
+   </div>
+  </div>
+
+  <div class="rounded-md flex flex-col shadow-xl">
+   <img src="https://media.giphy.com/media/OCeEYihTKpF2xtCywG/giphy.gif" alt="QR Generator Application Image"/>
+   <div class="p-4">
+    <h2 class="text-2xl underline underline-offset-2">Advice Generator</h2>
+    <p class="text-sm mt-2">An application that generates random advice</p>
+    <div class="flex space-x-3 mt-6">
+     <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
+     <a href="https://advice-generator-fm-challenge.netlify.app/" class="underline text-orange-500" target="_blank">Live Link</a>
+    </div>
+   </div>
+  </div>
+
+  <div class="rounded-md flex flex-col shadow-xl">
+   <img src="https://media.giphy.com/media/0fwIOkT8VMlwXgyG1P/giphy.gif" alt="QR Generator Application Image"/>
+   <div class="p-4">
+    <h2 class="text-2xl underline underline-offset-2">IP Address Locator</h2>
+    <p class="text-sm mt-2">An application that allows users to search for the name of their Internet Service provider and their location. </p>
+    <div class="flex space-x-3 mt-6">
+     <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
+     <a href="https://unique-parfait-415faf.netlify.app/" class="underline text-orange-500" target="_blank">Live Link</a>
+    </div>
+   </div>
+  </div>
+  
+  <div class="rounded-md flex flex-col shadow-xl">
+   <img src="https://media.giphy.com/media/ZwyN2CoxtQflNBofHh/giphy.gif" alt="QR Generator Application Image"/>
+   <div class="p-4">
+    <h2 class="text-2xl underline underline-offset-2">Tip Calculator</h2>
+    <p class="text-sm mt-2">A simple QR Code Generator built with Vuejs, Tailwindcss and Typescript</p>
+    <div class="flex space-x-3 mt-6">
+     <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
+     <a href="https://marvelous-empanada-972eae.netlify.app/" class="underline text-orange-500" target="_blank">Live Link</a>
+    </div>
+   </div>
+  </div>
+
+  <div class="rounded-md flex flex-col shadow-xl">
+   <img src="https://media.giphy.com/media/cNUwH9IoKyo6XvOsdH/giphy.gif" alt="QR Generator Application Image"/>
+   <div class="p-4">
+    <h2 class="text-2xl underline underline-offset-2">DC Marvel Hero Finder</h2>
+    <p class="text-sm mt-2">A simple QR Code Generator built with Vuejs, Tailwindcss and Typescript</p>
+    <div class="flex space-x-3 mt-6">
+     <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
+     <a href="https://dcmarvelherofinder.netlify.app/" class="underline text-orange-500" target="_blank">Live Link</a>
+    </div>
+   </div>
+  </div>
+
+  <div class="rounded-md flex flex-col shadow-xl">
+   <img src="https://media.giphy.com/media/QWPrqtS0UGX72KfLkM/giphy.gif" alt="QR Generator Application Image"/>
+   <div class="p-4">
+    <h2 class="text-2xl underline underline-offset-2">Expense Chart Component</h2>
+    <p class="text-sm mt-2">A simple QR Code Generator built with Vuejs, Tailwindcss and Typescript</p>
+    <div class="flex space-x-3 mt-6">
+     <a href="https://github.com/IanMcbull/QRCode-Reader/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
+     <a href="https://expense-chart-component-fem.netlify.app/" class="underline text-orange-500" target="_blank">Live Link</a>
     </div>
    </div>
   </div>
@@ -42,17 +125,6 @@
     <div class="flex space-x-3 mt-6">
      <a href="https://github.com/IanMcbull/tonnys-barbershop-website/tree/master" class="underline text-orange-500" target="_blank">Github Link</a>
      <a href="https://shavesandtrimz.onrender.com/" class="underline text-orange-500" target="_blank">Live Link</a>
-    </div>
-   </div>
-  </div>
-  <div class="rounded-md shadow-xl">
-   <img src="https://media.giphy.com/media/mtOVegCZ6sbrKCFqpy/giphy.gif" alt="Kates Kitchen Website Image"/>
-   <div class="p-4">
-    <h2 class="text-2xl underline underline-offset-2">Kate's Kitchen</h2>
-    <p class="mt-2 text-sm">A simple template Website Built using ExpressJS and EJS</p>
-    <div class="flex space-x-3 mt-6">
-     <a href="https://github.com/IanMcbull/recipe-website-repo" class="underline text-orange-500" target="_blank">Github Link</a>
-     <a href="https://salty-shelf-47963.herokuapp.com/" class="underline text-orange-500" target="_blank">Live Link</a>
     </div>
    </div>
   </div>
